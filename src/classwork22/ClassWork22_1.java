2280b747d4d97f747c37c65a596af227aa90712f
Dennis Schönberg <dennis_schoenberg@yahoo.de>
2023-10-30 20:58:51 +0100

    make addition calculator

--- src/classwork22/ClassWork22_1.java
+++ src/classwork22/ClassWork22_1.java
@@ -1,8 +1,14 @@
 package classwork22;
 
+import java.util.Scanner;
+
 public class ClassWork22_1 {
     public static void main(String[] args) {
-        System.out.println("Hello World++");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+        System.out.println("Ende");
     }
 
 }
